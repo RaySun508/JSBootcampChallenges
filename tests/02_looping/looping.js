2e30abee5ad585b3aebca93d00ad2a396248d2f3
Raychelle Lyman <raysuncreations@raychellesmbp2.lan>
2023-02-17 18:32:13 -0500

    str work

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,12 +1,28 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
+  
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
 
-}
 
 function stringReverse(str) {
 
-}
+    let reversed = '';
+  
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+  
+    return reversed;
+  }
+  
+
 
 // do not use built in slice method.
 function slicer(originalString, startIdx, endIdx) {
