7277a326fad668d28b459fef471b05a98b49a6db
Raychelle Lyman <raysuncreations@raychellesmbp2.lan>
2023-02-24 18:51:16 -0500

    worked on objects

--- tests/04_objects/objects.js
+++ tests/04_objects/objects.js
@@ -2,20 +2,24 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function deleteProp(obj, prop) {
-
+  delete obj[prop];
+  return obj;
 }
 
 function numObjectProps(obj) {
-
+  const totalItems = Object.keys(obj).length;
+ return totalItems;
 }
-
 function shoppingCart(cart) {
+  let total = 0
 
+  for (let i = 0; i < cart.length; i++)
+  const cartItem = cart[i];
+  total += cartItem.amount;
+  return total;
 }
 
 function compareObjects(obj1, obj2) {
-
-}
 
 module.exports = {
   compareObjects, deleteProp, numObjectProps, shoppingCart,
